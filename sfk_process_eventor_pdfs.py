df198da95861c1cc22fc0d371892ece64f6e4aee
m-alinder <129601068+m-alinder@users.noreply.github.com>
2024-12-08 21:57:59 +0100

    Add script for invoice processing
    Add script for processing pdf invoices received from eventor

---
+++ sfk_process_eventor_pdfs.py
@@ -0,0 +1,142 @@
+import argparse
+import pdfplumber
+import pandas as pd
+import re
+
+def read_pdf_files_in_directory(directory, outfile):
+    """
+    Läser in alla PDF-filer i en given katalog och skriver ut deras innehåll rad för rad.
+
+    Args:
+        directory (str): Sökvägen till katalogen.
+        outfile (str): Filnamn för resultat.
+    """
+
+    import os
+
+    dfError = pd.DataFrame(columns = ['Filnamn'])
+    df = pd.DataFrame(columns = ['Datum', 'Tävling', 'Namn', 'Klass', 'Tjänst', 'Avgift'])
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.pdf'):
+            #print(f"Processing file: {filename}")
+            file_path = os.path.join(directory, filename)
+            
+            try:
+                with pdfplumber.open(file_path) as pdf:
+                    event = ""
+                    eventDate = ""
+
+                    for page in pdf.pages:
+
+                        #print(page.extract_text(x_tolerance=2))
+                        text = page.extract_text(x_tolerance=2)
+                        # Skriv ut texten rad för rad
+                        for line in text.split("\n"):
+
+                            # Anmälan för <namn> i klass ...
+                            pattern = r"^.* f.r (.+) in? (.+) \d+,?\d* \d+,?\d* SEK (\d+,?\d*) SEK$"
+                            match = re.match(pattern, line)
+                            if match:
+                                name, clazz, cost = match.groups()
+                                df = pd.concat([df, pd.DataFrame([[eventDate, event, name, clazz, "", cost]], columns=['Datum', 'Tävling', 'Namn', 'Klass', 'Tjänst', 'Avgift'])], ignore_index=True)
+                                #print(f"{eventDate}, {event}, {name}, {clazz}, {cost}, anmälan")
+                                continue
+
+                            # Hyrbricka SportIdent för <namn>
+                            pattern = r"^(.+) f.r (.+) \d+,?\d* \d+,?\d* SEK (\d+,?\d*) SEK$"
+                            match = re.match(pattern, line)
+
+                            if match:
+                                subject, name, cost = match.groups()
+                                df = pd.concat([df, pd.DataFrame([[eventDate, event, name, "", subject, cost]], columns=['Datum', 'Tävling', 'Namn', 'Klass', 'Tjänst', 'Avgift'])], ignore_index=True)
+                                #print(f"{eventDate}, {event}, {name}, {subject}, {cost}, tjänst")
+                                continue
+
+                            # Tävling <tävlingsnamn>
+                            pattern = r"^Tävling +(.+)$"
+                            match = re.match(pattern, line)
+                            if match:
+                                #print(line)
+                                event = match.group(1)
+                                continue
+
+                            # Tävlingsdatum <datum>
+                            pattern = r"^Tävlingsdatum (.+)$"
+                            match = re.match(pattern, line)
+                            if match:
+                                #print(line)
+                                eventDate = match.group(1)
+                                if (event == ""):
+                                    event = filename[11:-4]
+                                continue
+                    
+                    if (event == "" or eventDate == ""):
+                        print(f"Kunde inte analysera: {filename}")
+                        dfError = pd.concat([dfError, pd.DataFrame([[filename]], columns=['Filnamn'])])
+
+            except Exception as e:
+                print(f"Error processing {filename}: {e}")
+
+    writer = pd.ExcelWriter(outfile, engine='xlsxwriter')
+
+    # Sort the table
+    df = df.sort_values(by=['Datum', 'Tävling', 'Namn'])
+
+    # Convert the dataframe to an XlsxWriter Excel object. We also turn off the
+    # index column at the left of the output dataframe.
+    df.to_excel(writer, sheet_name='Deltagare', index=False)
+
+    # Get the xlsxwriter workbook and worksheet objects.
+    #workbook  = writer.book
+    worksheet = writer.sheets['Deltagare']
+    worksheet.set_column(0,  0, 12)
+    worksheet.set_column(1,  1, 50)
+    worksheet.set_column(2,  2, 30)
+    worksheet.set_column(3,  3, 10)
+    worksheet.set_column(4,  4, 30)
+
+    dfSummary = df.drop(df[df.Klass == ""].index).groupby(['Datum', 'Tävling']).size().reset_index(name='Deltagare')
+    dfSummary.to_excel(writer, sheet_name='Tävlingar', index=False)
+    worksheet = writer.sheets['Tävlingar']
+    worksheet.set_column(0,  0, 12)
+    worksheet.set_column(1,  1, 50)
+    worksheet.set_column(2,  2, 10)
+
+
+    dfAmount = df.drop(df[df.Klass == ""].index).groupby(['Datum', 'Tävling', 'Avgift']).size().reset_index(name='Deltagare')
+    dfAmount['Nummer'] = dfAmount['Avgift'].str.replace(',', '.')
+    dfAmount['Nummer'] = dfAmount['Nummer'].astype(float)
+    dfAmount = dfAmount.sort_values(by=['Datum', 'Tävling', 'Nummer'])
+    dfAmount = dfAmount.drop('Nummer', axis=1)
+    dfAmount.to_excel(writer, sheet_name='Avgifter', index=False)
+    worksheet = writer.sheets['Avgifter']
+    worksheet.set_column(0,  0, 12)
+    worksheet.set_column(1,  1, 50)
+    worksheet.set_column(2,  2, 10)
+    worksheet.set_column(3,  3, 10)
+
+    dfService = df.drop(df[df.Tjänst == ""].index).groupby(['Datum', 'Tävling', 'Tjänst', 'Avgift']).size().reset_index(name='Antal')
+    dfService.to_excel(writer, sheet_name='Tjänster', index=False)
+    worksheet = writer.sheets['Tjänster']
+    worksheet.set_column(0,  0, 12)
+    worksheet.set_column(1,  1, 50)
+    worksheet.set_column(2,  2, 30)
+    worksheet.set_column(3,  4, 10)
+
+    dfError.to_excel(writer, sheet_name='Fel Format', index=False)
+    worksheet = writer.sheets['Fel Format']
+    worksheet.set_column(0,  0, 60)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.close()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_directory", type=str, help="Directory with received eventor invoices")
+parser.add_argument("output_file", type=str, help="Xlsx file to save result to")
+args = parser.parse_args()
+
+
+directory_path = "eventor-pdfs"  # Byt ut med din faktiska sökväg
+read_pdf_files_in_directory(args.input_directory, args.output_file)
